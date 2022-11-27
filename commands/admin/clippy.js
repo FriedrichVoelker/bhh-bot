e6ba045443f29f93d2ca68fe8a677acb9fc48829
FriedrichVoelker <friedrich.voelker@gmx.de>
2022-11-27 22:13:49 +0100

    Webhooks

---
+++ commands/admin/clippy.js
@@ -0,0 +1,37 @@
+const {SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('clippy')
+        .setDescription('Lässt Clippy eine Nachricht schreiben')
+        .setDefaultMemberPermissions( PermissionFlagsBits.ManageGuild)
+        .setDMPermission(false)
+        .addStringOption(option => option.setName('message').setDescription('Die Nachricht die Clippy schreiben soll').setRequired(true))
+        .addChannelOption(option => option.setName('channel').setDescription('Der Channel in dem Clippy schreiben soll').setRequired(false))
+        .addStringOption(option => option.setName('title').setDescription('Titel der Nachricht (Standard: Clippy)').setRequired(false)),
+    async execute(interaction) {
+        interaction.deferReply();
+        const message = interaction.options.getString('message');
+        const title = interaction.options.getString('title') || "Clippy";
+        const embed = new EmbedBuilder()
+            .setTitle(title)
+            .setColor("#00eaff")
+            .setDescription(message)
+            .setTimestamp()
+
+        const channel = interaction.options.getChannel('channel') || interaction.channel;
+        const webhooks = await channel.fetchWebhooks();
+
+        let webhook = webhooks.find(webhook => webhook.name === "Clippy");
+        if(!webhook) {
+            webhook = await channel.createWebhook({
+                name: "Clippy",
+                avatar: './static/images/clippy.jpeg',
+            });
+        }
+        webhook.send({
+            embeds: [embed],
+        });
+        await interaction.deleteReply();
+    },
+};
---
+++ commands/admin/webhook.js
@@ -0,0 +1,47 @@
+const {SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('webhook')
+        .setDescription('Sendet eine Nachricht über einen Webhook')
+        .setDefaultMemberPermissions( PermissionFlagsBits.ManageWebhooks)
+        .setDMPermission(false)
+        .addStringOption(option => option.setName('message').setDescription('Die Nachricht die der Webhook schreiben soll').setRequired(true))
+        .addStringOption(option => option.setName('name').setDescription('Der Name des Webhooks').setRequired(false))
+        .addChannelOption(option => option.setName('channel').setDescription('Der Channel in dem der Webhook schreiben soll').setRequired(false))
+        .addStringOption(option => option.setName("color").setDescription("Die Farbe des Embeds (Standard: #00eaff)").setRequired(false))
+        .addStringOption(option => option.setName("avatar").setDescription("Der Avatar des Webhooks").setRequired(false))
+        .addStringOption(option => option.setName('title').setDescription('Titel der Nachricht (Standard: Information)').setRequired(false)),
+    async execute(interaction, client) {
+        interaction.deferReply();
+        const message = interaction.options.getString('message');
+        const title = interaction.options.getString('title') || "Information";
+        const color = interaction.options.getString('color') || "#00eaff";
+        const avatar = interaction.options.getString('avatar') || client.user.displayAvatarURL();
+        const name = interaction.options.getString('name') || client.user.username;
+
+        const embed = new EmbedBuilder()
+            .setTitle(title)
+            .setColor(color)
+            .setDescription(message)
+            .setTimestamp()
+
+        const channel = interaction.options.getChannel('channel') || interaction.channel;
+        const webhooks = await channel.fetchWebhooks();
+
+        let webhook = webhooks.find(webhook => webhook.name === name);
+        if(!webhook) {
+            webhook = await channel.createWebhook({
+                name: name,
+                avatarURL: avatar,
+            });
+        }
+        await webhook.send({
+            embeds: [embed],
+            avatarURL: avatar,
+            name: name,
+        });
+        await webhook.delete();
+        await interaction.deleteReply();
+    }
+};
