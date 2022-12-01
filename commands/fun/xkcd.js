b94c8977d7fe88ea2953d2ed7fd23cd4315d6579
FriedrichVoelker <friedrich.voelker@gmx.de>
2022-12-02 00:08:57 +0100

    Added random xkcd

--- commands/fun/xkcd.js
+++ commands/fun/xkcd.js
@@ -5,10 +5,31 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('xkcd')
         .setDescription('Zeigt ein xkcd Comic an')
-        .addStringOption(option => option.setName('comic').setDescription('Der Comic der angezeigt werden soll')),
+        .addStringOption(option => option.setName('comic').setDescription('Der Comic der angezeigt werden soll'))
+        .addBooleanOption(option => option.setName('current').setDescription('Zeigt den neusten Comic')),
     async execute(interaction, client) {
-        let comic = interaction.options.getString('comic') ? "/" + interaction.options.getString('comic')  : '';
-        const response = await fetch(`https://xkcd.com${comic}/info.0.json`);
+        let queryString = "";
+
+        if(interaction.options.getBoolean('current')){
+            queryString = "https://xkcd.com/info.0.json";
+        } else if(interaction.options.getString('comic')){
+            queryString = "https://xkcd.com/" + interaction.options.getString('comic') + "/info.0.json";
+        } else {
+            const currentComic = await fetch("https://xkcd.com/info.0.json").then(response => response.json());
+            const randomComic = randomInt(currentComic.num);
+            queryString = "https://xkcd.com/" + randomComic + "/info.0.json";
+        }
+
+        
+        
+        const response = await fetch(queryString);
+        if(response.status != 200){
+            interaction.reply({content: "Der Comic konnte nicht gefunden werden", ephemeral: true});
+            return;
+        }
+        
+        
+        
         const body = await response.json();
         const embed = new EmbedBuilder()
             .setTitle(body.safe_title)
@@ -19,4 +40,8 @@ module.exports = {
             .setTimestamp()
         interaction.reply({embeds: [embed]});
     }
+}
+
+function randomInt(max) {
+    return Math.floor(Math.random() * (max - 1 + 1)) + 1;
 }
